1bcfc6cbdd8f9613c9b82ffd6dd07bab365b814a
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2022-11-28 16:44:42 +0200

    fed images to display from array

--- src/Gallerybody.js
+++ src/Gallerybody.js
@@ -4,25 +4,35 @@ import client2 from "./client2.png";
 import radarLogo from "./beambase.png";
 import { gsap } from "gsap";
 
+let imgArray = [client1, client2];
+
 function Gallerybody() {
   useEffect(() => {
     let imageBox = document.getElementById("image-box");
     let beam = document.getElementById("beam-base");
+    let srcNum = 0;
+    let imgSrc;
 
     beam.addEventListener("click", () => {
-      console.log("Clicked");
-
+      if (srcNum == imgArray.length - 1) {
+        srcNum = 0;
+      } else {
+        srcNum = srcNum + 1;
+      }
+      console.log(srcNum);
+      imgSrc = imgArray[srcNum];
+      //console.log(imgArray.length);
       gsap
         .timeline()
         .to("#image-box", {
-          x: "-100vw",
+          x: "-50vw",
           ease: "expo.out",
           opacity: 0,
           duration: 1,
         })
         .set("#image-box", {
-          x: "100vw",
-          attr: { src: client2 },
+          x: "50vw",
+          attr: { src: imgSrc },
         })
         .to("#image-box", {
           x: "0",
@@ -36,7 +46,7 @@ function Gallerybody() {
     <>
       <div className="gallery-center">
         <div className="image-boxes">
-          <img id="image-box" src={client1} />
+          <img id="image-box" src={client1} alt="client-site" />
         </div>
         <img src={radarLogo} alt="TheRadar Logo" id="beam-base" />
       </div>
