f0786fe14862a541cfcda4d4c0f7fc1d929efe02
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2022-11-30 22:54:34 +0200

    marquee edits

--- src/Servicesbody.js
+++ src/Servicesbody.js
@@ -18,20 +18,24 @@ function Servicebody() {
             alt="Man on the moon"
             src={moonman}
           />{" "}
-          Graphics Design and Branding{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Business Process Automation{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Application Security and Consultation{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Mobile App Development{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Web Application Development{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Discovery Enhancement Services{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
-          Content Writing/Development{" "}
-          <img className="marquee-img" alt="Man on the moon" src={moonman} />{" "}
+          Graphics Design and Branding
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+          Business Process Automation
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+        </p>
+      </div>
+      <div id="marquee">
+        <p id="marquee-content">
+          Application Security and Consultation
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+          Mobile App Development
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+          Web Application Development
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+          Discovery Enhancement Services
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
+          Content Writing/Development
+          <img className="marquee-img" alt="Man on the moon" src={moonman} />
         </p>
       </div>
       <button className="btn">Get a quote</button>
