266bd07fa96a5cb45c249ff568155a0b0cbfeeaf
6060l4 <bowojori7@gmail.com>
2023-01-19 01:42:46 -0800

    Removed the abominable image

---
+++ src/Gogreenfooter.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function Gogreenfooter() {
+  return (
+    <footer className="footerSection">
+      <div className="notice">
+        <p>
+          GO GREEN!
+        </p>
+      </div>
+    </footer>
+  );
+}
+export default Gogreenfooter;
