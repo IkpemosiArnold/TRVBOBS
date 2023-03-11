9b117fa49bc8ff401dacded04104a2531b00b241
6060l4 <bowojori7@gmail.com>
2023-03-10 20:30:53 -0800

    Reverted

--- src/Aboutslider.js
+++ src/Aboutslider.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "testHorizontalScroll.css";
 
 
 const settings = {
